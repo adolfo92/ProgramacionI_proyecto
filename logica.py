6aa4627bd619e517a0bb5a5cc7019bde7dfb7f5d
Adolfo92 <adolfo92dev@gmail.com>
2024-02-07 20:58:27 -0400

    Modifica sistema de conexion
    Cada llamado a metodo del objeto ListaAlumnos que pretenda interactuar con la base de datos, va a ser el encargado de abrir y cerrar su propia conexion a esta

--- logica.py
+++ logica.py
@@ -2,9 +2,8 @@
 
 import sqlite3
 
-miConexion = sqlite3.connect("./Databases/listaAlumnos")
-
-miCursor = miConexion.cursor()
+# miConexion = sqlite3.connect("./Databases/listaAlumnos")
+# miCursor = miConexion.cursor()
 
 
 class ListaAlumnos:
@@ -12,6 +11,10 @@ class ListaAlumnos:
 	alumnos = []
 
 	def __init__(self):
+		# Inicia conexion con base de datos
+		miConexion = sqlite3.connect("./Databases/listaAlumnos")
+		miCursor = miConexion.cursor()
+
 		# Creo Tabla a trabajar
 		try:
 			miCursor.execute('''
@@ -28,42 +31,65 @@ class ListaAlumnos:
 		miCursor.execute("SELECT * FROM ALUMNOS")
 		self.alumnos = miCursor.fetchall()
 
+		# Cierra la conexion
+		miConexion.close()
+
 	def actualizaLista(self):
+		# Inicia conexion con base de datos
+		miConexion = sqlite3.connect("./Databases/listaAlumnos")
+		miCursor = miConexion.cursor()
+
 		# Cargo el conenido actualizado de la lista a la lista "alumnos"
 		miCursor.execute("SELECT * FROM ALUMNOS")
 		self.alumnos = miCursor.fetchall()
 
+		# Cierra la conexion
+		miConexion.close()
+
 	def agregaPersonas(self, nombre, apellido, cedula):
+		# Inicia conexion con base de datos
+		miConexion = sqlite3.connect("./Databases/listaAlumnos")
+		miCursor = miConexion.cursor()
+
 		# Agrega personas y me actualiza la lista del objeto de una vez
 		try:
 			miCursor.execute(f"INSERT INTO ALUMNOS VALUES (NULL,?,?,?)", (nombre, apellido, cedula))
 		except:
 			return 0
 		miConexion.commit()
+		# Cierra la conexion
+		miConexion.close()
 		self.actualizaLista()
 
 	def eliminaPersonas(self, cedula):
+		# Inicia conexion con base de datos
+		miConexion = sqlite3.connect("./Databases/listaAlumnos")
+		miCursor = miConexion.cursor()
+
 		# Elimina personas y me actualiza la lista del objeto de una vez
-		print("DELETE FROM ALUMNOS WHERE CEDULA=" + str(cedula))
 		miCursor.execute(f"DELETE FROM ALUMNOS WHERE CEDULA=" + str(cedula))
 		miConexion.commit()
 		self.actualizaLista()
 
+		# Cierra la conexion
+		miConexion.close()
+
 	def modificaPersonas(self, cedula, campo, nuevoValor):
+		# Inicia conexion con base de datos
+		miConexion = sqlite3.connect("./Databases/listaAlumnos")
+		miCursor = miConexion.cursor()
+
 		# Modifica personas y me actualiza la tabla del objeto de una vez
 		miCursor.execute(f"UPDATE ALUMNOS SET {campo}=? WHERE CEDULA=?", (nuevoValor, cedula))
 		miConexion.commit()
 		self.actualizaLista()
+
+		# Cierra la conexion
+		miConexion.close()
 
 	def dameTabla(self):
 		# Me retorna el objeto lista
 		return self.alumnos
 
 
-miNuevaLista = ListaAlumnos()
 
-
-print(miNuevaLista.dameTabla())
-
-
-miConexion.close()
